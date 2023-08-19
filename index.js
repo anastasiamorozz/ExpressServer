73e30ff78d18d1a3bf1b861e82064ed0a41d23d8
Anastasiia Moroz <95378736+simpokot@users.noreply.github.com>
2023-08-19 13:36:24 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Server is runing");
+})
+
+app.listen(3000, ()=>{
+    console.log('server running on port 3000')});
